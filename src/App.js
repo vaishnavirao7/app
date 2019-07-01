aee8025dbaf610e3b4ace91d67260c97696bac4b
Vaishnavi Rao B <vairaob@publicisgroupe.net>
2019-07-01 14:49:09 +0530

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Badge from 'react-bootstrap/Badge';
 
 function App() {
   return (
@@ -19,6 +20,9 @@ function App() {
           Learn React
         </a> */}
         Hi! My name is Vaishnavi.
+        <Badge pill variant="warning">
+          Made with React-Bootstrap!
+        </Badge>
       </header>
     </div>
   );
